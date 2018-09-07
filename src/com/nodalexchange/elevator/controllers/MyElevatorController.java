80db00202e2c789e31a1a3223e0c2a3425bcfb29
Anup Jasani <momesmo@vt.edu>
2018-09-06 20:33:15 -0400

    Need to define the evaluate method and create sound logic

--- src/com/nodalexchange/elevator/controllers/MyElevatorController.java
+++ src/com/nodalexchange/elevator/controllers/MyElevatorController.java
@@ -9,8 +9,10 @@ import java.util.Vector;
 public class MyElevatorController implements ElevatorController {
     private enum CycleCases {INITIALIZE, COMPUTE}
     private CycleCases cycleSwitch = CycleCases.INITIALIZE;
-    private int numFloors = 0;
-    private int numElevators = 0;
+    private int numFloors = -1;
+    private int numElevators = -1;
+    private int topFloor = -1;
+    private int bottomFloor = -1;
     private boolean[] waitingAreaUpPushed;
     private boolean[] waitingAreaDownPushed;
     private Elevator[] elevatorArray;
@@ -24,7 +26,19 @@ public class MyElevatorController implements ElevatorController {
         for(int i = 0; i < numElevators; i++){
             elevatorArray[i] = building.getElevator(i);
             elevatorButtonsPushed[i] = elevatorArray[i].getButtonStates();
+            evaluateAndMove(elevatorArray[i].getElevatorNumber(), elevatorArray[i].getCurrentFloor(),
+                    waitingAreaUpPushed, waitingAreaDownPushed, elevatorButtonsPushed[i],
+                    elevatorArray[i].getButtonToAnswerAtNextStop());
         }
+    }
+
+    private void evaluateAndMove(int elevatorNumber, int currentFloor, boolean[] upPushed, boolean[] downPushed,
+                                 boolean[] floorsPushed, Elevator.DirectionEnum direction){
+
+//        System.out.printf("Elevator %d, Floor %d, Moving %s, with these buttons pushed:\n%s\n" +
+//                "Waiting to go up: %s\nWaiting to go down: %s\n\n", elevatorNumber, currentFloor,
+//                String.valueOf(direction), Arrays.toString(floorsPushed), Arrays.toString(upPushed),
+//                Arrays.toString(downPushed));
     }
 
     public void cycleElapsed(Building building) {
@@ -32,6 +46,8 @@ public class MyElevatorController implements ElevatorController {
             case INITIALIZE:
                 numElevators = building.getNumberOfElevators();
                 numFloors = building.getNumberOfFloors();
+                topFloor = numFloors - 1;
+                bottomFloor = 0;
                 waitingAreaUpPushed = new boolean[numFloors];
                 waitingAreaDownPushed = new boolean[numFloors];
                 elevatorArray = new Elevator[numElevators];
@@ -39,8 +55,6 @@ public class MyElevatorController implements ElevatorController {
                 cycleSwitch = CycleCases.COMPUTE;
             case COMPUTE:
                 getWaitingAreaAndElevatorButtons(building);
-
-
 
                 break;
         }
